d7d7f61fbbfaf8e058219babc026128f4078f83a
D5error <D5error@163.com>
2025-04-24 10:32:25 +0800

    update

--- kernel/trap.c
+++ kernel/trap.c
@@ -33,9 +33,7 @@ trapinithart(void)
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
 //
-void
-usertrap(void)
-{
+void usertrap(void) {
   int which_dev = 0;
 
   if((r_sstatus() & SSTATUS_SPP) != 0)
